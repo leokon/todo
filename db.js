267bbb2108cb3cca662400bbd1b778621770d957
Leo Kontogiorgis <leokon95@gmail.com>
2019-03-25 23:42:46 +1100

    Setup docker-compose database config

--- db.js
+++ db.js
@@ -2,11 +2,11 @@ const initOptions = {};
 
 const pgp = require('pg-promise')(initOptions);
 const db = pgp({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_DATABASE,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DB,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD
 });
 
 module.exports = db;
