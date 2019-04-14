e769c9f9591d10dd2cf524d1d1a1cbf8ae27d95b
Leo Kontogiorgis <leokon95@gmail.com>
2019-04-15 00:46:40 +1000

    Add visible feedback on filter tag selection

--- client/src/components/Tag.js
+++ client/src/components/Tag.js
@@ -4,7 +4,7 @@ import styled from 'styled-components';
 const Container = styled.span`
     width: fit-content;
     max-width: 110px;
-    padding: 4px;
+    padding: 2px;
     border-radius: 4px;
     background-color: #1595ad;
     color: #fff;
@@ -13,6 +13,8 @@ const Container = styled.span`
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
+    
+    border: ${props => props.selected ? '2px solid black' : '2px solid transparent'};
 `;
 
 class Tag extends React.Component {
@@ -23,7 +25,7 @@ class Tag extends React.Component {
 
     render() {
         return (
-            <Container onClick={this.props.onClick} title={this.props.tag.name}>
+            <Container onClick={this.props.onClick} title={this.props.tag.name} selected={this.props.selected}>
                 {this.props.tag.name}
             </Container>
         );
